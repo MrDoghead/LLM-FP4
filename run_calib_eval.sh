5f2483d2095825792e67db2898692b12ed4f4d45
MrDoghead <dongnancao@gmail.com>
2024-01-16 09:46:05 +0000

    change data path

--- run_calib_eval.sh
+++ run_calib_eval.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-MODEL_ADDR=/data/huggyllama/llama-7b
+MODEL_ADDR=/data/models/huggyllama/llama-7b
 
 python main.py --model hf-causal-experimental \
 	--model_args pretrained=$MODEL_ADDR,use_accelerate=True \
@@ -17,7 +17,7 @@ python main.py --model hf-causal-experimental \
 	--output_path /workspace/LLM-FP4/logs/run_calib_eval_results.json \
 	--write_out \
 	--output_base_path /workspace/LLM-FP4/logs/ \
-	--ptq_param_path "./q_params/FP4_FPQ.pt" \
+	--ptq_param_path "/data/models/LLM-FP4/q_params/FP4_FPQ.pt" \
 
 #--limit 128 \
 #--search_intervals 0.01 1.2 100 \
